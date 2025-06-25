bf254320bdbe9149c809926577c83a5a272dee01
sansui233 <sansuilnm@gmail.com>
2025-06-25 19:53:58 +0800

    去重

--- prepare_containerize.rb
+++ prepare_containerize.rb
@@ -6,6 +6,8 @@ MAPPING_FILE_DIRECTORY = './directory_mappings/'
 FINAL_SANDBOX_FILE = 'Containerize.wsb'
 
 require 'fileutils'
+require 'yaml'
+require 'set'
 
 def ensure_folder(path = '')
   abs_path = File.expand_path(path, './')
@@ -17,16 +19,21 @@ def process_entry(sandbox, host, ro)
   abs_path = ensure_folder(host)
   mapping_xml = MAPPED_FOLDER_TEMPLATE.gsub('{{sandbox}}', sandbox)
   mapping_xml.gsub!('{{host}}', abs_path)
-  mapping_xml.gsub!('{{ro}}', ro ? 'True' : 'False')
+  mapping_xml.gsub!('{{ro}}', ro ? 'true' : 'false')
   mapping_xml
 end
 
-require 'yaml'
+$sandbox_set = Set.new
 
 def process_mapping_file(filename)
   mapping_list = []
   mapping_obj = YAML.load_file(filename)
   mapping_obj.each do |entry|
+    if $sandbox_set.include?(entry['sandbox'])
+      warn "Warning: Duplicate sandbox path skipped: #{entry['sandbox']} in file #{filename}"
+      next
+    end
+    $sandbox_set << entry['sandbox']
     mapping_list << process_entry(entry['sandbox'], entry['host'], entry['ro'])
   end
   mapping_list
@@ -42,4 +49,3 @@ end
 mapping_folder_xml = final_mapping_list.join("\n")
 final_xml = SANDBOX_TEMPLATE.gsub('{{OtherMappedFolders}}', mapping_folder_xml)
 File.open(FINAL_SANDBOX_FILE, 'wt') { |f| f.write(final_xml) }
-
